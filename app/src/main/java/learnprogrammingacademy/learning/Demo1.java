cb37a8861d99b28ba7ba05b9818c5a192319f39b
Delta0077 <adas0262@gmail.com>
2020-11-28 01:08:53 +0530

    Update Demo1.java

--- app/src/main/java/learnprogrammingacademy/learning/Demo1.java
+++ app/src/main/java/learnprogrammingacademy/learning/Demo1.java
@@ -2,8 +2,17 @@ package learnprogrammingacademy.learning;
 
 public class Demo1 {
 
+
+    static String[] pets = {"parrots", "cat", "dogs"};
+
+    static int [] [] table={
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+    };
+
     public static void main(String[] args) {
-        
+
 
     }
 }
