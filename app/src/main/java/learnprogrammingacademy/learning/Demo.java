156710319986a51f73b38f13f6d8c1164344f1ff
Delta0077 <adas0262@gmail.com>
2020-11-27 21:38:21 +0530

    Update Demo.java

--- app/src/main/java/learnprogrammingacademy/learning/Demo.java
+++ app/src/main/java/learnprogrammingacademy/learning/Demo.java
@@ -6,14 +6,46 @@ public class Demo {
     static int lives=3;
     static int score;// 0 by default
     static int coinCount = 90;
+    static boolean gameOver; // false by default
 
-    static int itemCount = 24;
-    static int itemsPerPage = 5;
+   /* static int itemCount = 24;
+    static int itemsPerPage = 5;*/
 
 
     public static void main(String[] args) {
 
-        System.out.println("score " + score);
+        score+= 10;
+        coinCount = 110; // re-initialising
+       // lives = 0;
+
+        if(lives== 0)
+        {
+            gameOver= true;
+        }
+        if(coinCount>100)
+        {
+            System.out.println("gold medal");
+        }
+        else if(coinCount> 80){
+            System.out.println("silver medal");
+        }
+        else if(coinCount> 70){
+            System.out.println("bronze medal");
+        }
+        else{
+            System.out.println("no medal");
+        }
+        if(gameOver){
+            System.out.println("score " + score);
+        }
+        else{
+            System.out.println("lives " + lives);
+        }
+        if(!gameOver){
+            System.out.println("game is not over");
+        }
+
+      /*  System.out.println("score " + score);
 
         score+= 10;
         System.out.println("Score " + score + " lives " + lives);
@@ -34,6 +66,6 @@ public class Demo {
 
         //calculating remaining items
         int remainingItems = itemCount % itemsPerPage;
-        System.out.println("Remaining items " + remainingItems);
+        System.out.println("Remaining items " + remainingItems);*/
     }
 }
