86f16131ba5d71aaade015f1b79d270f2a009722
Delta0077 <adas0262@gmail.com>
2020-11-28 01:43:50 +0530

    Update Demo.java

--- app/src/main/java/learnprogrammingacademy/learning/Demo.java
+++ app/src/main/java/learnprogrammingacademy/learning/Demo.java
@@ -8,7 +8,12 @@ import java.sql.SQLOutput;
 
 
      static String[] pets= {"sparrow","parrot","crow"};
-     static int [] [] table = new int[3][3];
+     static int [] [] table = {
+
+             {1,2,3},
+             {4,5,6},
+             {7,8,9}
+     };
 
 
      public static void main(String[] args) {
