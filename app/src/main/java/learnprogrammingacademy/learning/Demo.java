1069acbd5a131fe1a399b1482fe22fe9aab7ab86
Delta0077 <adas0262@gmail.com>
2020-11-29 01:59:52 +0530

    Update Demo.java

--- app/src/main/java/learnprogrammingacademy/learning/Demo.java
+++ app/src/main/java/learnprogrammingacademy/learning/Demo.java
@@ -20,8 +20,36 @@ import java.sql.SQLOutput;
 
      public static void main(String[] args) {
 
+         String dog = "Dog";
+         String dog2 = new String(new char[]{'D','o','g'});
+
+         System.out.println("dog == dog2 => "+ (dog == dog2));
+         System.out.println("dog.equals(dog2) => " + dog.equals(dog2));
+
+         String cat ="Cat";
+         String cat2 = new String("Cat");
+         String cat3 = cat2; // 2 references(cat2 , cat3) to same object in memory
+
+         System.out.println("reference equality => " + (cat == cat2));//false
+         System.out.println("reference equality => " + (cat == cat3));//false
+         System.out.println("reference equality => " + (cat2 == cat3));//true
+
+         String lizard = "Lizard";
+         System.out.println("lizard first letter = " + lizard.charAt(0));
+         System.out.println("lizard contains iz = " + lizard.contains("iz"));
+
+         //immutability
+         lizard.toUpperCase();//this does not change string it creates new string
+
+         System.out.println("lizard = " +lizard);
+
+         lizard = lizard.toUpperCase();// assign new values to variable
+         System.out.println("lizard = " + lizard);
+
+
+
          //Break and continue statement use
-       /*  String [] animals = {"Dog","Cat","Lizard","Bird","Snake"};
+         String [] animals = {"Dog","Cat","Lizard","Bird","Snake"};
          for(String animal: animals) {
              if (animal.equals("Lizard")) {
                 break;
@@ -39,18 +67,18 @@ import java.sql.SQLOutput;
          }
 
          //my own code for challenge
-        /* int sum=0;
+         int sum=0;
          for(int i =1; i<=100; i++)
             sum+=i;
          {
              System.out.println("Sum = " +sum );
 
-         }*/
+         }
          //while
-      /*   int number = 1;
-         int sum = 0;
+         int number = 1;
+         int sum1 = 0;
          while(number<=100){
-             sum+= number;
+             sum1+= number;
              number++;
          }
          System.out.println("Sum " + sum);
@@ -74,7 +102,7 @@ import java.sql.SQLOutput;
          }
 
 
-       /*     for(int i = 0; i<pets.length;i++)
+            for(int i = 0; i<pets.length;i++)
             {
                 //1st iteration i = 0
                 //2nd iteration i = 1
@@ -95,6 +123,6 @@ import java.sql.SQLOutput;
 
                         System.out.println("number = " + table[row][column]);
 
-                 }*/
+                 }
     }
 }
