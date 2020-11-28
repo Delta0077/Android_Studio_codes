165bc9e929684468f2aed63b7ee9bba170855d24
Delta0077 <adas0262@gmail.com>
2020-11-29 00:23:43 +0530

    Update Demo.java

--- app/src/main/java/learnprogrammingacademy/learning/Demo.java
+++ app/src/main/java/learnprogrammingacademy/learning/Demo.java
@@ -11,7 +11,7 @@ import java.sql.SQLOutput;
      static int [] [] table = {
 
              {1,2,3},
-             {4,5,6},
+             {4,5,6,11},
              {7,8,9}
      };
 
@@ -19,10 +19,22 @@ import java.sql.SQLOutput;
      public static void main(String[] args) {
 
 
+            for(int i = 0; i<pets.length;i++)
+            {
+                //1st iteration i = 0
+                //2nd iteration i = 1
+                //3rd iteration i = 2
+                //4th iteration i=3 , 3<3, loop false
+                System.out.println("pet[" + i + "]= " + pets[i]);
+
+            }
+
+                for(String pet : pets){
+
+                    System.out.println("pet= " + pet);
 
 
-
-
-
+                }
+                    
     }
 }
